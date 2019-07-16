e6b956ff182bf56fa1fa69e13905b52f452bebd0
Steven Clark <stevenclark@Stevens-Air.lan1>
2019-07-15 21:30:50 -0500

    Updated question 3

--- question 3/question3.js
+++ question 3/question3.js
@@ -4,14 +4,13 @@ var rgbSection = document.getElementById("rgb_inputs");
 var hexSection = document.getElementById("hex_input");
 var processSelected = null;
 var formSubmit = document.getElementById("formSubmit");
-var conversionForm = document.getElementById("conversionForm");
 var finalResult = "nothing";
 
 
 rgbSelected.addEventListener("click", function() {
     rgbSection.classList.remove("hide");
     hexSection.classList.add("hide");
-    processSelected = "RGB";
+    processSelected = "rgb";
 });
 
 hexSelected.addEventListener("click", function() {
@@ -25,6 +24,7 @@ formSubmit.addEventListener("click", function() {
     var conversionResultsDiv = document.getElementById("conversionResults");
     conversionResultsDiv.classList.remove("hide");
     conversionResultsDiv.innerHTML = `${finalResult}`;
+    formSubmit.classList.add("hide");
 
 });
 
@@ -38,19 +38,20 @@ var rgbToHex = function (rgb) {
   };
 
 var rgbToHexCombined = function () {
-    var rbg1Input = document.getElementById("rgb1input").value;
-    var rbg2Input = document.getElementById("rgb2input").value;
-    var rbg3Input = document.getElementById("rgb3input").value;
-    var converted1 = rgbToHex(rbg1Input);
-    var converted2 = rgbToHex(rbg2Input);
-    var converted3 = rgbToHex(rbg3Input);
+    var rgb1Input = document.getElementById("rgb1input").value;
+    var rgb2Input = document.getElementById("rgb2input").value;
+    var rgb3Input = document.getElementById("rgb3input").value;
+    var converted1 = rgbToHex(rgb1Input);
+    var converted2 = rgbToHex(rgb2Input);
+    var converted3 = rgbToHex(rgb3Input);
     var hexArray = [converted1,converted2,converted3]
-    finalResult = `#${hexArray.join()}`
+    finalResult = `#${hexArray[0]}${hexArray[1]}${hexArray[2]}`
     return false;
 }
 
   var hextoRGB = function (hex) {
-    var splitString = hex.split("");
+    var hexlc = hex.toUpperCase();
+    var splitString = hexlc.split("");
     var swapLetters = [];
     splitString.forEach(element => {
         switch(element) {
@@ -79,7 +80,6 @@ var rgbToHexCombined = function () {
                 swapLetters.push(element);
           }
     });
-
     var rgb1 = (Number(swapLetters[0]) * 16) + Number(swapLetters[1]);
     var rgb2 = (Number(swapLetters[2]) * 16) + Number(swapLetters[3]);
     var rgb3 = (Number(swapLetters[4]) * 16) + Number(swapLetters[5]);
@@ -91,7 +91,7 @@ var rgbToHexCombined = function () {
 
 var conversionResult = function () {
     var hexInput = document.getElementById("hexinput").value;
-    if(processSelected === "RGB") {
+    if(processSelected === "hex") {
         hextoRGB(hexInput);
         return false;
     } else {
